cce2dc9f424497dea8626059c2c968d5345c3e73
apelsin apelsin <abrikosechek@gmail.com>
2024-12-28 02:45:50 +0400

    cubes throw

--- src/pages/Dice/Dice.vue
+++ src/pages/Dice/Dice.vue
@@ -3,6 +3,10 @@
     <div ref="cardComponent" id="card" class="card">
       <canvas ref="canvasComponent" />
     </div>
+
+    <Button @click="createCube">
+      Throw
+    </Button>
   </div>
 </template>
 
@@ -19,18 +23,18 @@ const canvasComponent = ref()
 onMounted(() => {
   initScene()
   createGround()
-  createCube()
   renderScene()
 })
 
 // scene/world variables
 let scene, camera, renderer, world
+let ambientLight, pointLight
 // object variables
 let ground0, groundBody0
 let ground1, groundBody1
 let ground2, groundBody2
 let ground3, groundBody3
-let cube, cubeBody
+const cubes = []
 
 const getRandomNumber = (min, max) => {
   return Math.random() * (max - min) + min;
@@ -44,14 +48,24 @@ const initScene = () => {
   scene = new THREE.Scene()
   camera = new THREE.PerspectiveCamera(75, width / height, 0.1, 1000)
 
+  ambientLight = new THREE.AmbientLight(0xffffff, 0.2)
+  scene.add(ambientLight)
+
+  pointLight = new THREE.PointLight(0xffffff, 1000)
+  pointLight.position.x = 0
+  pointLight.position.z = 0
+  pointLight.position.y = 30
+  scene.add(pointLight)
+
   renderer = new THREE.WebGLRenderer({
-    canvas: canvasComponent.value
+    canvas: canvasComponent.value,
+    antialias: true
   })
   renderer.setSize(width, height)
 
   // cannon-es
   world = new CANNON.World({
-    gravity: new CANNON.Vec3(0, - 0.91, 0)
+    gravity: new CANNON.Vec3(0, -1.91, 0)
   })
 }
 
@@ -61,7 +75,7 @@ const createGround = () => {
 
   // three object
   const groundGeometry0 = new THREE.PlaneGeometry(30, 30)
-  const groundMaterial0 = new THREE.MeshBasicMaterial({
+  const groundMaterial0 = new THREE.MeshStandardMaterial({
     color: color,
     side: THREE.DoubleSide,
     wireframe
@@ -79,7 +93,7 @@ const createGround = () => {
 
   // three object
   const groundGeometry1 = new THREE.PlaneGeometry(30, 20)
-  const groundMaterial1 = new THREE.MeshBasicMaterial({
+  const groundMaterial1 = new THREE.MeshStandardMaterial({
     color: color,
     side: THREE.DoubleSide,
     wireframe
@@ -97,7 +111,7 @@ const createGround = () => {
 
   // three object
   const groundGeometry2 = new THREE.PlaneGeometry(30, 20)
-  const groundMaterial2 = new THREE.MeshBasicMaterial({
+  const groundMaterial2 = new THREE.MeshStandardMaterial({
     transparent: true,
     opacity: 0,
     side: THREE.DoubleSide,
@@ -115,7 +129,7 @@ const createGround = () => {
 
   // three object
   const groundGeometry3 = new THREE.PlaneGeometry(30, 20)
-  const groundMaterial3 = new THREE.MeshBasicMaterial({
+  const groundMaterial3 = new THREE.MeshStandardMaterial({
     color: color,
     side: THREE.DoubleSide,
     wireframe
@@ -134,50 +148,49 @@ const createGround = () => {
 }
 
 const createCube = () => {
+  const cubeObject = {
+    mesh: null,
+    body: null
+  }
   const geometrySize = 4
 
   // three object
-  let geometry = new RoundedBoxGeometry(geometrySize, geometrySize, geometrySize, 5, 1);
-  console.log(geometry)
-  const positionAttribute = geometry.attributes.position;
+  let geometry = new RoundedBoxGeometry(geometrySize, geometrySize, geometrySize, 4, 1);
 
-  console.log(positionAttribute)
+  const textureLoader = new THREE.TextureLoader()
+  const materials = ['1', "2", "3", "4", "5", "6"]
+    .map((item) =>
+    (new THREE.MeshStandardMaterial({
+      map: textureLoader.load(`dice/${item}.png`)
+    }))
+    )
 
-  for (let i = 0; i < positionAttribute.count; i++) {
-
-    let position = new THREE.Vector3().fromBufferAttribute(positionAttribute, i);
-
-    // modify position.x, position.y and position.z
-
-    positionAttribute.setXYZ(i, position.x, position.y, position.z);
-  }
-
-  const material = new THREE.MeshBasicMaterial({ color: 0xc9c9c9, wireframe: false });
-
-  cube = new THREE.Mesh(geometry, material);
-  scene.add(cube);
+  cubeObject.mesh = new THREE.Mesh(geometry, materials);
+  scene.add(cubeObject.mesh);
 
   // cannon object
-  cubeBody = new CANNON.Body({
+  cubeObject.body = new CANNON.Body({
     mass: 1,
     shape: new CANNON.Box(new CANNON.Vec3(geometrySize / 2, geometrySize / 2, geometrySize / 2)),
-    position: new CANNON.Vec3(20, 10, 0)
+    position: new CANNON.Vec3(10, 6, 0)
   })
-  const moveImpulse = new CANNON.Vec3(-8, -4, 0)
-  cubeBody.applyImpulse(moveImpulse)
+  const moveImpulse = new CANNON.Vec3(-8, -4, getRandomNumber(-3, 2))
+  cubeObject.body.applyImpulse(moveImpulse)
 
   const angularVelocity = new CANNON.Vec3(getRandomNumber(-3, 2), Math.PI * getRandomNumber(-3, 3), getRandomNumber(-3, 2)); // Rotating around Y-axis
-  cubeBody.angularVelocity.copy(angularVelocity);
+  cubeObject.body.angularVelocity.copy(angularVelocity);
 
-  world.addBody(cubeBody)
+  world.addBody(cubeObject.body)
+
+  cubes.push(cubeObject)
 }
 
 const renderScene = () => {
   //   camera final
   camera.position.x = -5;
-  camera.position.y = 17;
-  camera.position.z = 12;
-  camera.rotation.x = -1;
+  camera.position.y = 21;
+  camera.position.z = 6;
+  camera.rotation.x = -1.3;
   //   camera far
   // camera.position.y = 40;
   // camera.position.z = 30;
@@ -191,8 +204,10 @@ const renderScene = () => {
     world.step(1 / 60)
 
     // link objects
-    cube.position.copy(cubeBody.position)
-    cube.quaternion.copy(cubeBody.quaternion)
+    cubes.forEach((cube) => {
+      cube.mesh.position.copy(cube.body.position)
+      cube.mesh.quaternion.copy(cube.body.quaternion)
+    })
     //
     ground0.position.copy(groundBody0.position)
     ground0.quaternion.copy(groundBody0.quaternion)
@@ -211,10 +226,19 @@ const renderScene = () => {
 
   renderer.setAnimationLoop(animate)
 }
+
+const rerenderScene = () => {
+  renderer.clear();
+  renderer.render(scene, camera);
+}
 </script>
 
 <style scoped lang='scss'>
 .page {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 12px;
   width: 100%;
   max-width: 350px;
   margin: 0 auto;
@@ -222,16 +246,9 @@ const renderScene = () => {
 
 .card {
   width: 100%;
-  aspect-ratio: 1/1.2;
+  aspect-ratio: 1/1;
   background: var(--bg-light);
   border-radius: 4px;
   overflow: hidden;
-}
-
-.buttons {
-  display: grid;
-  grid-template-columns: repeat(2, 1fr);
-  margin-top: 4px;
-  gap: 4px;
 }
 </style>
